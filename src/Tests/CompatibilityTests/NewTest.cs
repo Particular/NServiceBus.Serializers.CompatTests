4f38ff85ec9b05b1cb5c4b664511a86d4006db95
Michał Wójcik <wojcik.mike@gmail.com>
2021-02-25 19:54:46 +0100

    Adding a test to deserialize

--- src/Tests/CompatibilityTests/NewTest.cs
+++ src/Tests/CompatibilityTests/NewTest.cs
@@ -45,5 +45,34 @@
 
             }
         }
+
+        static string[] NserviceBusVersions = new[]
+        {
+            "NServiceBus6.1",
+            "NServiceBus6.2"
+        };
+
+        [TestCaseSource(nameof(NserviceBusVersions))]
+        public void Deserialize(string version)
+        {
+            var projectDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\"));
+            //TODO: Discover all test versions
+
+            //TODO: we need to discover the available target platforms for each project somehow
+            var workingDir = Path.Combine(projectDirectory, version, "bin", ConfigurationFolder, "net452");
+            var execPath = Path.Combine(workingDir, version + ".exe");
+            try
+            {
+                //TODO Running just the executable from the correct working directory doesn't seem to work
+                //Run(version + ".exe", args: "Serialize", workingDirectory: workingDir);
+                //TODO redirecting standard output would be nice
+                Run(execPath, args: "Deserialize");
+            }
+            catch (Exception e)
+            {
+                TestContext.WriteLine(e);
+                throw new Exception("Failed to run deserialization for version " + version, e);
+            }
+        }
     }
 }
