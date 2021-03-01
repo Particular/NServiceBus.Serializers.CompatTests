f3023a7574f7cac2de8540410a3cc6e8e992440c
Michał Wójcik <wojcik.mike@gmail.com>
2021-03-01 12:13:05 +0100

    Change Serialization tests to be like Deserialization (use Read instead of Run, and use parameters)

--- src/Tests/CompatibilityTests/NewTest.cs
+++ src/Tests/CompatibilityTests/NewTest.cs
@@ -19,35 +19,26 @@
         const string ConfigurationFolder = "Release";
 #endif
 
+        [TestCaseSource(nameof(NserviceBusVersions))]
         [Test]
-        public void Serialize()
+        public void Serialize(string version)
         {
             var projectDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\"));
-            //TODO: Discover all test versions
-            var nserviceBusVersions = new[]
-            {
-                "NServiceBus6.1",
-                "NServiceBus6.2"
-            };
 
-            foreach (var version in nserviceBusVersions)
+            //TODO: we need to discover the available target platforms for each project somehow
+            var workingDir = Path.Combine(projectDirectory, version, "bin", ConfigurationFolder, "net452");
+            var execPath = Path.Combine(workingDir, version + ".exe");
+            try
             {
-                //TODO: we need to discover the available target platforms for each project somehow
-                var workingDir = Path.Combine(projectDirectory, version, "bin", ConfigurationFolder, "net452");
-                var execPath = Path.Combine(workingDir, version + ".exe");
-                try
-                {
-                    //TODO Running just the executable from the correct working directory doesn't seem to work
-                    //Run(version + ".exe", args: "Serialize", workingDirectory: workingDir);
-                    //TODO redirecting standard output would be nice
-                    Run(execPath, args: "Serialize");
-                }
-                catch (Exception e)
-                {
-                    TestContext.WriteLine(e);
-                    throw new Exception("Failed to run serialization for version " + version, e);
-                }
-
+                //TODO Running just the executable from the correct working directory doesn't seem to work
+                //Run(version + ".exe", args: "Serialize", workingDirectory: workingDir);
+                //TODO redirecting standard output would be nice
+                Console.Write(Read(execPath, args: "Serialize"));
+            }
+            catch (Exception e)
+            {
+                TestContext.WriteLine(e);
+                throw new Exception("Failed to run serialization for version " + version, e);
             }
         }
 
