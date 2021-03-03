af21c059d29bef816bce17d7bf09669f7ec22983
Michał Wójcik <wojcik.mike@gmail.com>
2021-03-03 12:21:39 +0100

    Replacing SimpleExec with CLIWrap

--- src/Tests/CompatibilityTests/DeserializationTests.cs
+++ src/Tests/CompatibilityTests/DeserializationTests.cs
@@ -1,25 +1,30 @@
 ﻿namespace Tests.CompatibilityTests
 {
-    using static SimpleExec.Command;
     using System;
     using System.IO;
+    using System.Text;
+    using System.Threading.Tasks;
+    using CliWrap;
     using NUnit.Framework;
 
     [TestFixture, Order(2)]
     public class DeserializationTests
     {
         [TestCaseSource(typeof(TestCaseGenerator), nameof(TestCaseGenerator.NServiceBusVersions))]
-        public void Deserialize(TestDescription testDescription)
+        public async Task Deserialize(TestDescription testDescription)
         {
             var execPath = Path.Combine(testDescription.Directory, testDescription.Name + ".exe");
+            var standardOutput = new StringBuilder();
+            var errorOutput = new StringBuilder();
             try
             {
-                //TODO redirecting standard output would be nice
-                Console.Write(Read(execPath, args: "Deserialize"));
+                await Cli.Wrap(execPath).WithArguments("Deserialize").WithStandardOutputPipe(PipeTarget.ToStringBuilder(standardOutput)).WithStandardErrorPipe(PipeTarget.ToStringBuilder(errorOutput)).ExecuteAsync();
+                Console.WriteLine(standardOutput.ToString());
             }
             catch (Exception e)
             {
                 TestContext.WriteLine(e);
+                Console.WriteLine(errorOutput.ToString());
                 throw new Exception("Failed to run deserialization for version " + testDescription.Name, e);
             }
         }
--- src/Tests/CompatibilityTests/SerializationTests.cs
+++ src/Tests/CompatibilityTests/SerializationTests.cs
@@ -1,8 +1,10 @@
 ﻿namespace Tests.CompatibilityTests
 {
-    using static SimpleExec.Command;
     using System;
     using System.IO;
+    using System.Text;
+    using System.Threading.Tasks;
+    using CliWrap;
     using Common;
     using NUnit.Framework;
 
@@ -16,17 +18,20 @@
         }
 
         [TestCaseSource(typeof(TestCaseGenerator), nameof(TestCaseGenerator.NServiceBusVersions))]
-        public void Serialize(TestDescription testDescription)
+        public async Task Serialize(TestDescription testDescription)
         {
             var execPath = Path.Combine(testDescription.Directory, testDescription.Name + ".exe");
+            var standardOutput = new StringBuilder();
+            var errorOutput = new StringBuilder();
             try
             {
-                //TODO redirecting standard output would be nice
-                Console.Write(Read(execPath, args: "Serialize"));
+                await Cli.Wrap(execPath).WithArguments("Serialize").WithStandardOutputPipe(PipeTarget.ToStringBuilder(standardOutput)).WithStandardErrorPipe(PipeTarget.ToStringBuilder(errorOutput)).ExecuteAsync();
+                Console.WriteLine(standardOutput.ToString());
             }
             catch (Exception e)
             {
                 TestContext.WriteLine(e);
+                Console.WriteLine(errorOutput.ToString());
                 throw new Exception("Failed to run serialization for version " + testDescription.Name, e);
             }
         }
