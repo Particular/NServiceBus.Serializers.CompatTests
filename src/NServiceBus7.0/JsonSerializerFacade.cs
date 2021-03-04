c58d872cb169d75181a7f7df972faa94b99c6ef1
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-03-04 13:33:57 +0100

    add NServiceBus 7 test project

---
+++ src/NServiceBus7.0/JsonSerializerFacade.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using Common;
+using Common.Tests;
+using NServiceBus;
+using NServiceBus.MessageInterfaces.MessageMapper.Reflection;
+using NServiceBus.Serialization;
+using NServiceBus.Settings;
+
+class JsonSerializerFacade : ISerializerFacade
+{
+    public JsonSerializerFacade(params Type[] objectTypes)
+    {
+        this.objectTypes = objectTypes;
+        mapper = new MessageMapper();
+        mapper.Initialize(objectTypes);
+        var settings = new SettingsHolder();
+        serializer = new NewtonsoftSerializer().Configure(settings)(mapper);
+    }
+
+    public SerializationFormat SerializationFormat => SerializationFormat.Json;
+
+    public void Serialize(Stream stream, object instance)
+    {
+        serializer.Serialize(new[]
+        {
+            instance
+        }, stream);
+    }
+
+    public object[] Deserialize(Stream stream)
+    {
+        return serializer.Deserialize(stream, objectTypes);
+    }
+
+    public object CreateInstance(Type type)
+    {
+        return type.IsInterface ? mapper.CreateInstance(type) : Activator.CreateInstance(type);
+    }
+
+    MessageMapper mapper;
+    IMessageSerializer serializer;
+    Type[] objectTypes;
+}
