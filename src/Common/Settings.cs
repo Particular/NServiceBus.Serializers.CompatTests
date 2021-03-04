4e75ef1f5a471a3046cd58d2df0a2d1e81e61777
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-03-04 11:10:16 +0100

    use system temp path

--- src/Common/Settings.cs
+++ src/Common/Settings.cs
@@ -4,7 +4,6 @@
 
     public class Settings
     {
-        //TODO where should we store the files?
-        public static readonly DirectoryInfo TestDirectory = Directory.CreateDirectory(Path.Combine(Directory.GetDirectoryRoot(Directory.GetCurrentDirectory()), "temp", "serializer-compat-tests"));
+        public static readonly DirectoryInfo TestDirectory = Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), "serializer-compat-tests"));
     }
 }
