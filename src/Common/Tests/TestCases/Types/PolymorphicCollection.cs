a6e56a76ef0399e81ad8079eecbc12e9ae4ded4d
Andreas Öhlund <andreas.ohlund@particular.net>
2021-08-20 09:49:49 +0200

    Fix naming

--- src/Common/Tests/TestCases/Types/PolymorphicCollection.cs
+++ src/Common/Tests/TestCases/Types/PolymorphicCollection.cs
@@ -4,7 +4,7 @@
     using System.Collections.Generic;
 
     [Serializable]
-    public class PolymorphicCollection
+    public class Polymorphic
     {
         public List<BaseEntity> Items { get; set; }
     }
