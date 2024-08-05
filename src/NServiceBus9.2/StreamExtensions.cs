0c6ab161d2514b107e0528a52cbd2f3bd55716d8
Dan Kent <dan.kent@particular.net>
2024-08-05 14:44:38 -0400

    Add NServiceBus.9.2-alpha.1 tests

---
+++ src/NServiceBus9.2/StreamExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+static class StreamExtensions
+{
+    public static ReadOnlyMemory<byte> ReadFully(this Stream s)
+    {
+        using (var memoryStream = new MemoryStream())
+        {
+            s.CopyTo(memoryStream);
+            return new ReadOnlyMemory<byte>(memoryStream.ToArray());
+        }
+    }
+}
+
