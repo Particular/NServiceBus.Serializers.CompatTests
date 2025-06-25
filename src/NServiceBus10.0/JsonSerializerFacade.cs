92e0d7cbaa416653aac0c700f1bee2ce189ccdde
Brandon Ording <bording@gmail.com>
2025-06-25 18:14:51 -0400

    Add NServiceBus 10 project

---
+++ src/NServiceBus10.0/JsonSerializerFacade.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using Common;
+using Common.Tests;
+using Newtonsoft.Json;
+using NServiceBus;
+using NServiceBus.MessageInterfaces.MessageMapper.Reflection;
+using NServiceBus.Serialization;
+using NServiceBus.Settings;
+
+class JsonSerializerFacade : ISerializerFacade
+{
+    public JsonSerializerFacade(params Type[] objectTypes)
+    {
+        this.objectTypes = objectTypes;
+        mapper = new MessageMapper();
+        mapper.Initialize(objectTypes);
+        var settings = new SettingsHolder();
+
+        var jsonSettings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto };
+        settings.Set("NServiceBus.Newtonsoft.Json.Settings", jsonSettings);
+        var ser = new NewtonsoftJsonSerializer();
+        serializer = ser.Configure(settings)(mapper);
+    }
+
+    public SerializationFormat SerializationFormat => SerializationFormat.Json;
+
+    public void Serialize(Stream stream, object instance)
+    {
+        serializer.Serialize(instance, stream);
+    }
+
+    public object[] Deserialize(Stream stream)
+    {
+        return serializer.Deserialize(stream.ReadFully(), objectTypes);
+    }
+
+    public object CreateInstance(Type type)
+    {
+        return type.IsInterface ? mapper.CreateInstance(type) : Activator.CreateInstance(type);
+    }
+
+    MessageMapper mapper;
+    IMessageSerializer serializer;
+    Type[] objectTypes;
+}
---
+++ src/NServiceBus10.0/SystemJsonSerializerFacade.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Common;
+using Common.Tests;
+using NServiceBus;
+using NServiceBus.MessageInterfaces.MessageMapper.Reflection;
+using NServiceBus.Serialization;
+using NServiceBus.Settings;
+
+class SystemJsonSerializerFacade : ISerializerFacade
+{
+    public SystemJsonSerializerFacade(params Type[] objectTypes)
+    {
+        this.objectTypes = objectTypes;
+        mapper = new MessageMapper();
+        mapper.Initialize(objectTypes);
+        var settings = new SettingsHolder();
+
+        var ser = new SystemJsonSerializer();
+        serializer = ser.Configure(settings)(mapper);
+    }
+
+    public SerializationFormat SerializationFormat => SerializationFormat.SystemJson;
+
+    public void Serialize(Stream stream, object instance)
+    {
+        serializer.Serialize(instance, stream);
+    }
+
+    public object[] Deserialize(Stream stream)
+    {
+        return serializer.Deserialize(stream.ReadFully(), objectTypes);
+    }
+
+    public object CreateInstance(Type type)
+    {
+        return type.IsInterface ? mapper.CreateInstance(type) : Activator.CreateInstance(type);
+    }
+
+    MessageMapper mapper;
+    IMessageSerializer serializer;
+    Type[] objectTypes;
+}
